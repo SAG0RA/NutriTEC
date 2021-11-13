baabda9242f0f0914c1d7a939b5b25faa907ceb5
IIISAGORAIII <saulgora9@gmail.com>
2021-11-13 00:35:15 -0600

    login cliente con SQLite
    send nudes

--- NutriTECMovil/app/src/main/java/com/sagora/nutritecmovil/API/RestAPIService.kt
+++ NutriTECMovil/app/src/main/java/com/sagora/nutritecmovil/API/RestAPIService.kt
@@ -2,6 +2,8 @@ package com.sagora.nutritecmovil.API
 
 import android.util.Log
 import com.sagora.nutritecmovil.API.Data.Clientes
+import com.sagora.nutritecmovil.Database.Cliente.ClienteDB
+import com.sagora.nutritecmovil.Funciones.Inicio
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -10,11 +12,15 @@ class RestAPIService {
     /**Funcion encargada de enviar la solicitud GET al REST API en /api/cliente
      * @param db: Base de datos donde se guardara la correspondiente info
      */
-    fun getClient() {
+    fun getClient(db: ClienteDB) {
         val retrofit = ServiceBuilder.buildService(RestAPI::class.java)
         retrofit.getClient().enqueue(object : Callback<List<Clientes>> {
             override fun onResponse(call: Call<List<Clientes>>, response: Response<List<Clientes>>) {
                 val datos = response.body()
+
+                if (datos != null) {
+                    Inicio().sync_Cliente(datos,db)
+                }
 
                 for (c in datos!!)
 // Print para verificar que se haya hecho bien la solicitud
@@ -22,7 +28,8 @@ class RestAPIService {
                         "CLIENTE: ",
                         "Cedula: ${c.cedula} " +
                                 "\n Nombre: ${c.nombre} " +
-                                "\n Apellido: ${c.p_apellido} "
+                                "\n Apellido: ${c.p_apellido}" +
+                                "\n Passw: ${c.passw}"
                     )
 
             }
